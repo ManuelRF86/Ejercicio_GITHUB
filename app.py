1f5aa4b0f7ffd0fecadbad91631edc0544328d4e
Manuel_Reina <manuelrf86@hotmail.com>
2023-10-18 17:26:15 +0200

    sin g

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-""" Hacemos Featuring Engineering """
+""" Hacemos Featuring Enineering """
 train_df["Sex"] = train_df["Sex"].map({"male": 0, "female": 1})
 test_df["Sex"] = test_df["Sex"].map({"male": 0, "female": 1})
 train_df["Sex"].fillna(train_df["Age"].mean(), inplace=True)
