d5f25e701ae3262383a6473b6efd389826b0a79e
Manuel_Reina <manuelrf86@hotmail.com>
2023-10-18 16:55:45 +0200

    codigo completo

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 '''
 Importamos librerías
 '''
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+Importamos librerías
+'''
 import pandas
 import numpy
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
+'''
+Cargamos los datos
+'''
+train_df = pd.read_csv('data/train.csv')
+test_df = pd.read_csv('data/test.csv')
