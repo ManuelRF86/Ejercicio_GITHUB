885abf4960bfd88b2f967d6b89a79a687ecf9e4d
Manuel_Reina <manuelrf86@hotmail.com>
2023-10-18 16:49:20 +0200

    parte2

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 '''
 Importamos librerías
 '''
-import streamlit as st
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas
+import numpy
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+st.set_option('deprecation.showPyplotGlobalUse', False)
